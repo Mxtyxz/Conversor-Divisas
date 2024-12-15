5c2c7aad2ced3e41d4b14cd7499e3a7b32abe195
Matias Peterka <matias.tauro@hotmail.com>
2024-12-15 18:07:39 -0300

    Final Project

---
+++ src/com/alura/conversor_divisas/Divisas.java
@@ -0,0 +1,11 @@
+package com.alura.conversor_divisas;
+
+import java.util.Map;
+
+public class Divisas {
+    private Map<String, Double> conversion_rates;
+
+    public Map<String, Double> getConversionRates() {
+        return conversion_rates;
+    }
+}
